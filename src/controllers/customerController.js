b84f68d0eea7660f8224049d8b0a1b3f095ff305
Amit Khemlani <amitkhemlani@gmail.com>
2025-11-25 22:51:03 -0500

    Your message

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -2,20 +2,33 @@ const Customer = require('../models/Customer');
 
 // Create a new customer
 exports.createCustomer = async (req, res) => {
-  //try {
+  try {
     const customer = new Customer(req.body);
     await customer.save();
-    res.status(201).json({
+    return res.status(201).json({
       success: true,
       message: 'Customer created successfully',
       data: customer
     });
-  /*} catch (error) {
-    res.status(400).json({
-      success: false,
-      message: error.message
-    });
-  }*/
+  } catch (error) {
+    // Duplicate key (unique index)
+    if (error && error.code === 11000) {
+      const keyVal = error.keyValue || {};
+      const field = Object.keys(keyVal)[0] || 'field';
+      const value = keyVal[field] || '';
+      return res.status(409).json({
+        success: false,
+        message: `${field} '${value}' already exists`
+      });
+    }
+
+    // Mongoose validation error
+    if (error && error.name === 'ValidationError') {
+      return res.status(400).json({ success: false, message: error.message });
+    }
+
+    return res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 // Get all customers
